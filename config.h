2d3faefff4fb4c659362a4e8533ccde8ec359fe2
fuzzmaker <lajilongba7@gmail.com>
2019-09-03 23:27:55 +0800

    新增基础头文件，链表实现，登录部分代码

---
+++ config.h
@@ -0,0 +1,128 @@
+/**********************************
+*
+* 基本配置文件-包涵项目所需头文件
+* 定义枚举类和用户链表结构体
+* 
+**********************************/
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<select.h>
+#include<pthread.h>
+#include<arpa/inet.h>
+#include "linked.h"
+
+#define SERVER_IP 127.0.0.1
+#define PORT 8888
+#define DB_FILE "/var/db/chatuser"
+#define BUF_MAXSIZE 8126
+
+/*定义描述符集大小*/
+#ifndef FD_SETSIZE
+#define FD_SETSIZE 256
+#endif
+
+
+/*是否禁言*/
+enum Flag{
+	YES, //被禁言
+	NO //未被禁言
+};
+
+/*消息类型枚举*/
+enum MessageType{
+	REGIST, //注册
+	LOGIN, //登录
+	HELP, //帮助
+	LISTUSERS, //查看在线用户列表
+	HISTRECORDS, //查看聊天记录
+	GROUPCHAT, //群聊消息
+	PRIVCHAT, //私聊消息
+	EXIT, //下线
+	RESULT, //结果消息
+	UNKNOWN //未知类型
+};
+
+/*消息结果状态枚举*/
+enum State{
+	CONN_LIMIT, //连接已达上线
+	SUCCESS, //成功
+	FAIL, //失败
+	DUPLICATE_NAME, //用户名重复
+	WRONGPWD, //密码错误
+	NO_ONE_ONLINE, //无人在线
+	NOT_ONLINE, //不在线
+	ALREADY_ONLINE //已经在线
+}
+
+/*消息结构体*/
+typedef struct _message{
+	char recvName[20]; //接收用户名
+	char sendName[20]; //发送用户名
+	int msgType; //消息类型
+	int state; //消息状态
+	char content[2048]; //消息内容
+	struct sockaddr_in recvAddr; //接收端地址
+	struct sockaddr_in sendAddr; //发送端地址
+	char sendTime[20]; //发送时间
+}Message;
+
+/*用户信息结构体*/
+typedef struct _user{
+	char name[20]; //用户名
+	char passwd[20]; //密码
+	int fd; //套接字描述符
+	int state; //状态
+	char regTime[20]; //注册时间
+}User;
+
+/*服务端接口定义*/
+/*消息分发处理*/
+void requestHandler(Message *msg,int fd);
+
+void login(Message msg,int fd);
+
+void groupChat(Message msg,int fd);
+
+void privateChat(Message msg,int fd);
+
+void registion(Message msg,int fd);
+
+void listUsers(Message msg,int fd);
+
+char *geterrmsg(int state);
+
+User *getUserByName(const char *name);
+
+/*客户端接口定义*/
+/*登录*/
+void login(Message *msg,int fd);
+
+/*进入聊天室*/
+void enterChat(int fd);
+
+/*注册*/
+void registion(int fd); 
+
+/*群聊*/
+void groupChat(int fd);
+
+/*私聊*/
+void privateChat(int fd);
+
+/*登出*/
+void logout(int fd);
+
+/*展示在线用户列表*/
+void listUsers(int fd);
+
+/*主界面提示*/
+void mainInterface();
+
+/*帮助信息*/
+void helpInterface();
+
+
---
+++ linked.c
@@ -0,0 +1,46 @@
+#include<stdlib.h>
+#include "linked.h"
+
+Node *create_linked(){
+	lk_size=0;
+	lk_head=NULL;
+	cur_node=NULL;
+	return lk_head;
+}
+
+void add(Node *linked,void *val){
+	Node *new_node=malloc(sizeof(struct Node));
+	new_node->val=val;
+	if(cur_node==NULL){
+		lk_head=new_node;
+		cur_node=new_node;
+	}else{
+		cur_node->next=new_node;
+	}
+	lk_size++;
+}
+
+void remove(Node *linked,void *val){
+	if(lk_size==0) return;
+	Node *temp=lk_head;
+	while(temp!=NULL){
+		if(temp->val==val){
+			Node *oNext=temp->next;
+			free(temp);
+			temp=oNext;
+			lk_size++;
+			break;	
+		}
+	}
+}
+
+
+void destroy(){
+	if(lk_size==0) return;
+	Node *temp=lk_head;
+	while(temp!=NULL){
+		Node *oNext=temp->next;
+		free(temp);
+		temp=oNext;
+	}
+}
